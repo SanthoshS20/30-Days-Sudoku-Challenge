82bafbad21748312cdb3b6417ef0c4ffaa0676f7
SanthoshS20 <santhoshsekar97@gmail.com>
2021-07-05 18:32:17 +0530

    Added Script to automate the update of time in README file

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # Sudoku-Challenge
 Post the Sudoku Daily Challenge with Timer
+
+Each Day with the Difficult Level and the Time Taken to complete the challenge
+
+
+Day      Difficult Level      Time(In Seconds)
+
+
+Day 1        Easy                  263
+
+
+Day 2        Easy                  203
+
+
+Day 3        Medium                  1275
+
+
+Day 4        Easy                  225
+
+
+Easy Level
+
+Easy Minimum Time Taken - 203
+
+Easy Maximum Time Taken - 263
+
+Average Time Taken - 230
+
+Medium Level
+
+Medium Minimum Time Taken - 1275
+
+Medium Maximum Time Taken - 1275
+
+Average Time Taken - 1275
---
+++ TimeRange.py
@@ -0,0 +1,102 @@
+lines = []
+with open('Time.txt') as f:
+    lines = f.readlines()
+f.close()
+
+total_lines = len(lines)
+
+TimeTakenInDays = dict()
+DifficultLevelInEachDays = dict()
+day = 1
+for index in range(total_lines):
+    if(index!=0):
+        difficultLevel, Time = lines[index].split(" ")
+        TimeTakenInDays["Day "+str(day-1)] = int(Time)
+        DifficultLevelInEachDays["Day "+str(day-1)] = difficultLevel
+    day+=1
+
+
+easyAverage = 0
+# 20 minutes
+easyMin = 1200
+easyMax = 0
+easyCount = 0
+
+mediumAverage = 0
+# 60 minutes
+mediumMin = 3600
+mediumMax = 0
+mediumCount = 0
+
+hardAverage = 0
+# 2 hours
+hardMin = 7200
+hardMax = 0
+hardCount = 0
+
+for index in range(total_lines-1):
+    key = "Day {}".format(index+1)
+    if(DifficultLevelInEachDays[key]=="Easy"):
+        easyAverage+=TimeTakenInDays[key]
+        if(TimeTakenInDays[key]>easyMax):
+            easyMax = TimeTakenInDays[key]
+        if(TimeTakenInDays[key]<easyMin):
+            easyMin = TimeTakenInDays[key]
+        easyCount+=1
+    elif(DifficultLevelInEachDays[key]=="Medium"):
+        mediumAverage+=TimeTakenInDays[key]
+        if(TimeTakenInDays[key]>mediumMax):
+            mediumMax = TimeTakenInDays[key]
+        if(TimeTakenInDays[key]<mediumMin):
+            mediumMin = TimeTakenInDays[key]
+        mediumCount+=1
+    elif(DifficultLevelInEachDays[key]=="Hard"):
+        hardAverage+=TimeTakenInDays[key]
+        if(TimeTakenInDays[key]>hardMax):
+            hardMax = TimeTakenInDays[key]
+        if(TimeTakenInDays[key]<hardMin):
+            hardMin = TimeTakenInDays[key]
+        hardCount+=1
+    
+lines = []
+with open("README.md", "r") as f:
+    lines = f.readlines()
+f.close()
+
+del lines[8:len(lines)]
+
+with open("README.md", "w") as f:
+    for line in lines:
+        f.write(line)
+f.close()
+
+
+with open("README.md", "a") as f:
+    for index in range(total_lines-1):
+        key = "Day {}".format(index+1)
+        f.write("\n"+key+"        ")
+        f.write(DifficultLevelInEachDays[key]+"                  ")
+        f.write(str(TimeTakenInDays[key]))
+        f.write("\n\n")
+    
+    if(easyCount!=0):
+        f.write("\nEasy Level\n")
+        f.write("\nEasy Minimum Time Taken - "+str(easyMin))
+        f.write("\n\nEasy Maximum Time Taken - "+str(easyMax))
+        f.write("\n\nAverage Time Taken - "+str(easyAverage//easyCount))
+
+    if(mediumCount!=0):
+        f.write("\n\nMedium Level\n")
+        f.write("\nMedium Minimum Time Taken - "+str(mediumMin))
+        f.write("\n\nMedium Maximum Time Taken - "+str(mediumMax))
+        f.write("\n\nAverage Time Taken - "+str(mediumAverage//mediumCount))
+
+    if(hardCount!=0):
+        f.write("\n\nHard Level\n")
+        f.write("\n\nHard Minimum Time Taken - "+str(hardMin))
+        f.write("\n\nHard Maximum Time Taken - "+str(hardMax))
+        f.write("\n\nAverage Time Taken - "+str(hardAverage//hardCount))
+
+f.close()
+
+  
